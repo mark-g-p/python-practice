1a7cc0286396ea7ec63ec8931fc50f345681e0fc
Marek P <marek.g.przybylowicz@gmail.com>
2020-11-03 20:27:33 +0100

    Get meta-data from id

--- xkcd.py
+++ xkcd.py
@@ -20,6 +20,17 @@ def current_comic():
  
 
 @app.route('/comics/<int:comic_id>')
-def comic_by_id():
-    pass 
+def comic_by_id(comic_id):
+    with urllib.request.urlopen(f"http://xkcd.com/{comic_id}/info.0.json") as url:
+        data = json.loads(url.read().decode())
+        release_date = datetime.strptime(data['year']+data['month']+data['day'], "%Y%m%d")        
+        result = {
+            "id": data['num'],
+            "description": data['transcript'],
+            "date": release_date.strftime('%y-%m-%d'),
+            "title": data['title'].lower(),
+            "url": data['img'],
+            }
+        return (result)
+ 
 
