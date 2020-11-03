0132b0f8633516ee5957f5e33c0c5b7184485c18
Marek P <marek.g.przybylowicz@gmail.com>
2020-11-03 20:18:46 +0100

    Get meta-data from current chapter

--- xkcd.py
+++ xkcd.py
@@ -1,9 +1,23 @@
 from flask import Flask
+import urllib.request, json
+from datetime import datetime
+
 app = Flask(__name__)
 
 @app.route('/comics/current')
 def current_comic():
-    pass
+    with urllib.request.urlopen("http://xkcd.com/info.0.json") as url:
+        data = json.loads(url.read().decode())
+        release_date = datetime.strptime(data['year']+data['month']+data['day'], "%Y%m%d")        
+        result = {
+            "id": data['num'],
+            "description": data['transcript'],
+            "date": release_date.strftime('%y-%m-%d'),
+            "title": data['title'].lower(),
+            "url": data['img'],
+            }
+        return (result)
+ 
 
 @app.route('/comics/<int:comic_id>')
 def comic_by_id():
