8bdd1dd5845e74bbbdb910ced4196e57213408fc
Marek P <marek.g.przybylowicz@gmail.com>
2020-11-04 12:29:57 +0100

    Refactor a bit

--- xkcd.py
+++ xkcd.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 import urllib.request, json
 from datetime import datetime
 
@@ -9,28 +9,30 @@ def current_comic():
     with urllib.request.urlopen("http://xkcd.com/info.0.json") as url:
         data = json.loads(url.read().decode())
         release_date = datetime.strptime(data['year']+data['month']+data['day'], "%Y%m%d")        
-        result = {
+        meta = {
             "id": data['num'],
             "description": data['transcript'],
             "date": release_date.strftime('%y-%m-%d'),
             "title": data['title'].lower(),
             "url": data['img'],
             }
-        return (result)
+        return meta, 200
  
 
 @app.route('/comics/<int:comic_id>')
+def get_comic_by_id(comic_id):
+    return comic_by_id(comic_id), 200
+
+
 def comic_by_id(comic_id):
     with urllib.request.urlopen(f"http://xkcd.com/{comic_id}/info.0.json") as url:
         data = json.loads(url.read().decode())
         release_date = datetime.strptime(data['year']+data['month']+data['day'], "%Y%m%d")        
-        result = {
+        meta = {
             "id": data['num'],
             "description": data['transcript'],
             "date": release_date.strftime('%y-%m-%d'),
             "title": data['title'].lower(),
             "url": data['img'],
             }
-        return (result)
- 
-
+        return (meta)
