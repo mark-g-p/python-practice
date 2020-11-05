7bfa5129454517b68de867fb614cb440925f89b6
Marek P <marek.g.przybylowicz@gmail.com>
2020-11-05 20:14:34 +0100

    Add error handling

--- xkcd.py
+++ xkcd.py
@@ -1,8 +1,12 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, abort
 import urllib.request, json
 from datetime import datetime
+import os
 
 app = Flask(__name__)
+
+PORT =  5000 if os.environ.get('FLASK_PORT') is None else os.environ.get('FLASK_PORT')
+HOST = '127.0.0.1' if os.environ.get('FLASK_HOST') is None else os.environ.get('FLASK_Host')
 
 @app.route('/comics/current', methods=['GET'])
 def current_comic():
@@ -22,21 +26,31 @@ def many_comics():
     return jsonify( [comic_by_url(create_url_from_id(comic_id)) for comic_id in comics_ids] )
        
 
+@app.route('/comics/<string:_>')
+def wrong_url(_):
+    abort(400)
+
+
 def create_url_from_id(comic_id):
     return f"http://xkcd.com/{comic_id}/info.0.json"
 
 
 def comic_by_url(comic_url):
-    with urllib.request.urlopen(comic_url) as url:
-        data = json.loads(url.read().decode())
-        release_date = datetime.strptime(data['year']+data['month']+data['day'], "%Y%m%d")        
-        
-        meta = {
-            "id": data['num'],
-            "description": data['transcript'],
-            "date": release_date.strftime('%y-%m-%d'),
-            "title": data['title'].lower(),
-            "url": data['img'],
-            }
-        return meta
+    try:
+        with urllib.request.urlopen(comic_url) as url:
+            data = json.loads(url.read().decode())
+            release_date = datetime.strptime(data['year']+data['month']+data['day'], "%Y%m%d")        
+            
+            meta = {
+                "id": data['num'],
+                "description": data['transcript'],
+                "date": release_date.strftime('%y-%m-%d'),
+                "title": data['title'].lower(),
+                "url": data['img'],
+                }
+            return meta
+    except urllib.error.HTTPError as e:
+        abort(e.code)
 
+if __name__ == '__main__':
+    app.run(port=PORT, host=HOST)
